07ba63aeacf29d1033a6853213bd9d4cb2a1760f
Angela Kerubo <nkerubo.angela@gmail.com>
2024-05-29 21:38:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ document.querySelectorAll('.smooth-scroll').forEach(anchor => {
         });
     });
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.querySelector('.toggle-button');
+    const navLinks = document.querySelector('.nav-links');
+
+    toggleButton.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+    // Add event listeners to each menu option
+    const menuOptions = document.querySelectorAll('.nav-links a');
+    menuOptions.forEach(option => {
+        option.addEventListener('click', () => {
+            navLinks.classList.remove('active');
+        });
+    });    
+});
+
